e64ade038293d8885c7ee4007fba2fae6bfc33bb
ole <ole.bueker@gmail.com>
2021-12-16 20:29:09 +0100

    premake
    Followed video #7

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "OloEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "OloEngine"
+	location "OloEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"OLO_PLATFORM_WINDOWS",
+			"OLO_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+		
+	filter "configurations:Debug"
+		defines "OLO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OLO_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OLO_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"OloEngine/vendor/spdlog/include",
+		"OloEngine/src"
+	}
+
+	links
+	{
+		"OloEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"OLO_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "OLO_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OLO_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OLO_DIST"
+		optimize "On"
