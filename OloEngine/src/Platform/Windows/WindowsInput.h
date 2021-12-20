17ba0aa0ba6e998a5baf0ce1445e136ee52aa23c
ole <ole.bueker@gmail.com>
2021-12-20 18:44:09 +0100

    Input polling
    Followed video#19

---
+++ OloEngine/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "OloEngine/Input.h"
+
+namespace OloEngine {
+
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+
+}
--- OloEngine/src/Platform/Windows/WindowsWindow.h
+++ OloEngine/src/Platform/Windows/WindowsWindow.h
@@ -21,6 +21,7 @@ namespace OloEngine {
 		void SetVSync(bool enabled) override;
 		bool IsVSync() const override;
 
+		inline virtual void* GetNativeWindow() const { return m_Window; }
 	private:
 		virtual void Init(const WindowProps& props);
 		virtual void Shutdown();
