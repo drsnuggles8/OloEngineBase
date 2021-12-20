5cb95c3b3ff43093cd48b89adbe72dff1ed08a13
ole <ole.bueker@gmail.com>
2021-12-20 17:26:13 +0100

    enabled asserts

--- OloEngine/src/OloEngine/Core.h
+++ OloEngine/src/OloEngine/Core.h
@@ -10,13 +10,18 @@
     #error OloEngine only supports Windows!
 #endif
 
-#ifdef OLO_ENABLED_ASSERTS
+#ifdef OLO_DEBUG
+    #define OLO_ENABLE_ASSERTS
+#endif
+
+#ifdef OLO_ENABLE_ASSERTS
     #define OLO_ASSERT(x, ...) { if (!(x)) { OLO_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
     #define OLO_CORE_ASSERT(x, ...) { if (!(x)) { OLO_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 #else
     #define OLO_ASSERT(x, ...)
     #define OLO_CORE_ASSERT(x, ...)
 #endif
+
 #define BIT(x) (1 << x)
 
 #define OLO_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
