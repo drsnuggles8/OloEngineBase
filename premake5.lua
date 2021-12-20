550327f5cd56de231d55f06f3692b997210d342c
ole <ole.bueker@gmail.com>
2021-12-20 18:00:36 +0100

    update premake again

--- premake5.lua
+++ premake5.lua
@@ -71,8 +71,7 @@ project "OloEngine"
 
 		postbuildcommands
 		{
-			("IF NOT EXIST ../bin/" .. outputdir .. "/Sandbox mkdir ../biin/" .. outputdir .. "/Sandbox"),
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 		
 	filter "configurations:Debug"
