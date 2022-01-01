180ff18b6cea6a45a80b329614eceb5b91627a7c
ole <ole.bueker@gmail.com>
2022-01-01 12:41:57 +0100

    Fixed typo in comments

--- OloEngine/src/OloEngine/Events/Event.h
+++ OloEngine/src/OloEngine/Events/Event.h
@@ -6,7 +6,7 @@
 namespace OloEngine {
 
 	// Events in OloEngine are currently blocking, meaning when an event occurs it
-	// immediately gets dispatched and must be dealt with right then an there.
+	// immediately gets dispatched and must be dealt with right then and there.
 	// For the future, a better strategy might be to buffer events in an event
 	// bus and process them during the "event" part of the update stage.
 
