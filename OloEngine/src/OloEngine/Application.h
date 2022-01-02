93441cd0226d5522559c230290de954a3e165792
ole <ole.bueker@gmail.com>
2022-01-02 21:58:53 +0100

    Drew first triangle

--- OloEngine/src/OloEngine/Application.h
+++ OloEngine/src/OloEngine/Application.h
@@ -34,6 +34,7 @@ namespace OloEngine {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
