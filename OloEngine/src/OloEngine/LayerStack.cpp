397a183e1424db5dc6b106d4028b08207589abfb
personal <ole.bueker@gmail.com>
2022-01-04 12:38:18 +0100

    OnDetach must be called before removing from stack

--- OloEngine/src/OloEngine/LayerStack.cpp
+++ OloEngine/src/OloEngine/LayerStack.cpp
@@ -30,9 +30,9 @@ namespace OloEngine {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if (it != m_Layers.end())
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
-			layer->OnDetach();
 		}
 	}
 
@@ -41,8 +41,8 @@ namespace OloEngine {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
 		{
-			m_Layers.erase(it);
 			overlay->OnDetach();
+			m_Layers.erase(it);
 		}
 	}
 
