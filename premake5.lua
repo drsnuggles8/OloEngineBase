36df9c1083ab32005be978fb35fa3b5ffca7e997
personal <ole.bueker@gmail.com>
2022-01-05 21:47:00 +0100

    Updated premake for multhreading and improved generate project

--- premake5.lua
+++ premake5.lua
@@ -9,6 +9,11 @@ workspace "OloEngine"
 		"Dist"
 	}
 
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
