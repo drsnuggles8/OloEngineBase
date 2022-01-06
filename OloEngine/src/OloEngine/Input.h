502eb32164ffd627019ad7c718a34acb671a3448
personal <ole.bueker@gmail.com>
2022-01-06 16:22:05 +0100

    Maintenance
    Input as singleton, no std::function in Event dispatcher

--- OloEngine/src/OloEngine/Input.h
+++ OloEngine/src/OloEngine/Input.h
@@ -5,7 +5,12 @@
 namespace OloEngine {
 	class OLO_API Input
 	{
+	protected:
+		Input() = default;
 	public:
+		Input(const Input&) = delete;
+		Input& operator=(const Input&) = delete;
+
 		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
 
 		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
