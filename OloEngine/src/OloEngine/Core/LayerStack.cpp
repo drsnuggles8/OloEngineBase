bbe31729d6b902b58a327f9d72182eb32b6bcc3d
personal <ole.bueker@gmail.com>
2022-01-07 14:11:58 +0100

    Call OnDetach before deleting

--- OloEngine/src/OloEngine/Core/LayerStack.cpp
+++ OloEngine/src/OloEngine/Core/LayerStack.cpp
@@ -9,7 +9,11 @@ namespace OloEngine {
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}
+			
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
