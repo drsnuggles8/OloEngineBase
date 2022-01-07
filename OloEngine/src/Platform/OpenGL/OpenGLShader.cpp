0395e96f278c3f0857f0a753ebb6d8f2de6d8603
personal <ole.bueker@gmail.com>
2022-01-07 15:15:08 +0100

    Added file reading check

--- OloEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ OloEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -53,10 +53,18 @@ namespace OloEngine {
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
-			result.resize(in.tellg());
-			in.seekg(0, std::ios::beg);
-			in.read(&result[0], result.size());
-			in.close();
+			size_t size = in.tellg();
+			if (size != -1)
+			{
+				result.resize(size);
+				in.seekg(0, std::ios::beg);
+				in.read(&result[0], size);
+				in.close();
+			}
+			else
+			{
+				OLO_CORE_ERROR("Could not read from file '{0}'", filepath);
+			}
 		}
 		else
 		{
