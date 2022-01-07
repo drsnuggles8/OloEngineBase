b74adb1a72c9101846cebc7b7f5ac659fa0c05f9
personal <ole.bueker@gmail.com>
2022-01-07 15:06:38 +0100

    Basic ref-counting system to terminate glfw

--- OloEngine/src/Platform/Windows/WindowsWindow.cpp
+++ OloEngine/src/Platform/Windows/WindowsWindow.cpp
@@ -8,7 +8,7 @@
 #include "Platform/OpenGL/OpenGLContext.h"
 
 namespace OloEngine {
-	static bool s_GLFWInitialized = false;
+	static uint8_t s_GLFWWindowCount = 0;
 
 	static void GLFWErrorCallback(int error, const char* description)
 	{
@@ -38,17 +38,16 @@ namespace OloEngine {
 
 		OLO_CORE_INFO("Creating window {0}, ({1}, {2})", props.Title, props.Width, props.Height);
 
-		if (!s_GLFWInitialized)
+		if (s_GLFWWindowCount == 0)
 		{
-			// TODO: glfwTerminate on system shutdown
+			OLO_CORE_INFO("Initializing GLFW");
 			int success = glfwInit();
 			OLO_CORE_ASSERT(success, "Could not initialize GLFW!");
 			glfwSetErrorCallback(GLFWErrorCallback);
-
-			s_GLFWInitialized = true;
 		}
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
+		++s_GLFWWindowCount;
 		
 		m_Context = CreateScope<OpenGLContext>(m_Window);
 
@@ -151,6 +150,12 @@ namespace OloEngine {
 	void WindowsWindow::Shutdown()
 	{
 		glfwDestroyWindow(m_Window);
+
+		if (--s_GLFWWindowCount == 0)
+		{
+			OLO_CORE_INFO("Terminating GLFW");
+			glfwTerminate();
+		}
 	}
 
 	void WindowsWindow::OnUpdate()
