a9051318563fe9b1c9e6a5dab73a27bb99b7a0e6
personal <ole.bueker@gmail.com>
2022-01-07 14:21:24 +0100

    Fixed edge case in OpenGLShader::PreProcess Logic
    1. Assert that nextLinePos is not std::string::npos.
    2. Split the final assign operation to let the substring run to the end of the file if pos is std::string::npos (if there is no next #type token).

--- OloEngine/src/Platform/OpenGL/OpenGLShader.cpp
+++ OloEngine/src/Platform/OpenGL/OpenGLShader.cpp
@@ -82,8 +82,10 @@ namespace OloEngine {
 			OLO_CORE_ASSERT(ShaderTypeFromString(type), "Invalid shader type specified");
 
 			size_t nextLinePos = source.find_first_not_of("\r\n", eol);
+			OLO_CORE_ASSERT(nextLinePos != std::string::npos, "Syntax error");
 			pos = source.find(typeToken, nextLinePos);
-			shaderSources[ShaderTypeFromString(type)] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
+
+			shaderSources[ShaderTypeFromString(type)] = (pos == std::string::npos) ? source.substr(nextLinePos) : source.substr(nextLinePos, pos - nextLinePos);
 		}
 
 		return shaderSources;
