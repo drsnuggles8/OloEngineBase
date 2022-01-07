aa525cfd0e51dc833ee6b6b1413b6f61f2ad2d6e
personal <ole.bueker@gmail.com>
2022-01-07 14:42:37 +0100

    Fixed orthographic camera controller movement

--- OloEngine/src/OloEngine/Renderer/OrthographicCameraController.h
+++ OloEngine/src/OloEngine/Renderer/OrthographicCameraController.h
@@ -19,6 +19,8 @@ namespace OloEngine {
 		OrthographicCamera& GetCamera() { return m_Camera; }
 		const OrthographicCamera& GetCamera() const { return m_Camera; }
 
+		float GetZoomLevel() const { return m_ZoomLevel; }
+		void SetZoomLevel(float level) { m_ZoomLevel = level; }
 	private:
 		bool OnMouseScrolled(MouseScrolledEvent& e);
 		bool OnWindowResized(WindowResizeEvent& e);
@@ -30,7 +32,7 @@ namespace OloEngine {
 		bool m_Rotation;
 
 		glm::vec3 m_CameraPosition = { 0.0f, 0.0f, 0.0f };
-		float m_CameraRotation = 0.0f;
+		float m_CameraRotation = 0.0f; //In degrees, in the anti-clockwise direction
 		float m_CameraTranslationSpeed = 5.0f, m_CameraRotationSpeed = 180.0f;
 	};
 
