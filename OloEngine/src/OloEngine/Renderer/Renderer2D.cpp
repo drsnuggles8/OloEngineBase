d89849a68d0e2498e2bfcf0c52bf139e9f80f7c8
personal <ole.bueker@gmail.com>
2022-01-11 21:57:45 +0100

    Fixed rendering an empty Renderer2D scene

--- OloEngine/src/OloEngine/Renderer/Renderer2D.cpp
+++ OloEngine/src/OloEngine/Renderer/Renderer2D.cpp
@@ -135,6 +135,9 @@ namespace OloEngine {
 
 	void Renderer2D::Flush()
 	{
+		if (s_Data.QuadIndexCount == 0)
+			return;
+
 		// Bind textures
 		for (uint32_t i = 0; i < s_Data.TextureSlotIndex; i++)
 			s_Data.TextureSlots[i]->Bind(i);
