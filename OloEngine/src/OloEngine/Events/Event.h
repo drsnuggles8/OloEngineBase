e38e489611823d522a0ef7f1bf94643511dda2ea
personal <ole.bueker@gmail.com>
2022-01-12 16:19:53 +0100

    Adding more virtual Destructors

--- OloEngine/src/OloEngine/Events/Event.h
+++ OloEngine/src/OloEngine/Events/Event.h
@@ -39,6 +39,8 @@ namespace OloEngine {
 	class Event
 	{
 	public:
+		virtual ~Event() = default;
+
 		bool Handled = false;
 
 		virtual EventType GetEventType() const = 0;
--- OloEngine/src/OloEngine/Renderer/GraphicsContext.h
+++ OloEngine/src/OloEngine/Renderer/GraphicsContext.h
@@ -5,6 +5,8 @@ namespace OloEngine {
 	class GraphicsContext
 	{
 	public:
+		virtual ~GraphicsContext() = default;
+
 		virtual void Init() = 0;
 		virtual void SwapBuffers() = 0;
 
