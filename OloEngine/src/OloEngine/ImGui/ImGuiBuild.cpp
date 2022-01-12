1104bbc422c794be660b6a199766a3a4434599b0
personal <ole.bueker@gmail.com>
2022-01-12 13:14:40 +0100

    PVS Studio Comments

--- OloEngine/src/OloEngine/ImGui/ImGuiBuild.cpp
+++ OloEngine/src/OloEngine/ImGui/ImGuiBuild.cpp
@@ -1,3 +1,5 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "OloEnginePCH.h"
 
 //#define _CRT_SECURE_NO_WARNINGS
