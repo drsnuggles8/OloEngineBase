d8a5b49e0e35feac9216c243bbfb7fe2ee7b606b
personal <ole.bueker@gmail.com>
2022-01-13 21:32:58 +0100

    Fixed invalid entt::entity equal entt::null, which is not equal to 0

--- OloEngine/src/OloEngine/Scene/Entity.h
+++ OloEngine/src/OloEngine/Scene/Entity.h
@@ -42,7 +42,7 @@ namespace OloEngine {
 
 		operator bool() const { return m_EntityHandle != entt::null; }
 	private:
-		entt::entity m_EntityHandle{ 0 };
+		entt::entity m_EntityHandle{ entt::null };
 		Scene* m_Scene = nullptr;
 	};
 
