b2f46063a2e059eba860bd522d69e959450da908
personal <ole.bueker@gmail.com>
2022-01-13 21:31:37 +0100

    Replaced std::bind in HZ_BIND_EVENT_FN macro

--- OloEngine/src/OloEngine/Core/Base.h
+++ OloEngine/src/OloEngine/Core/Base.h
@@ -68,7 +68,7 @@
 
 #define BIT(x) ((1) << (x))
 
-#define OLO_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define OLO_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace OloEngine {
 
