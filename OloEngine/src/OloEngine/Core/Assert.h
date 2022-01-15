083088de17f84d7015cd684c754f3252ac62f9f4
personal <ole.bueker@gmail.com>
2022-01-15 20:23:54 +0100

    Replace filename extraction with equivalent standard library function

--- OloEngine/src/OloEngine/Core/Assert.h
+++ OloEngine/src/OloEngine/Core/Assert.h
@@ -2,31 +2,15 @@
 
 #include "OloEngine/Core/Base.h"
 #include "OloEngine/Core/Log.h"
+#include <filesystem>
 
 #ifdef OLO_ENABLE_ASSERTS
-
-	namespace OloEngine::Assert
-	{
-		// Returns the simple file name rather than full path as suggested by LovelySanta
-		constexpr const char* CurrentFileName(const char* path)
-		{
-			const char* file = path;
-			while (*path)
-			{
-				if (*path == '/' || *path == '\\')
-					file = ++path;
-				else
-					path++;
-			}
-			return file;
-		}
-	}
 
 	// Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 	#define OLO_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { OLO##type##ERROR(msg, __VA_ARGS__); OLO_DEBUGBREAK(); } }
 	#define OLO_INTERNAL_ASSERT_WITH_MSG(type, check, ...) OLO_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-	#define OLO_INTERNAL_ASSERT_NO_MSG(type, check) OLO_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", OLO_STRINGIFY_MACRO(check), ::OloEngine::Assert::CurrentFileName(__FILE__), __LINE__)
+	#define OLO_INTERNAL_ASSERT_NO_MSG(type, check) OLO_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", OLO_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 	#define OLO_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 	#define OLO_INTERNAL_ASSERT_GET_MACRO(...) OLO_EXPAND_MACRO( OLO_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, OLO_INTERNAL_ASSERT_WITH_MSG, OLO_INTERNAL_ASSERT_NO_MSG) )
