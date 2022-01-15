4f0c307cab514f76c1adb8ff6162b7e7306bcb3d
personal <ole.bueker@gmail.com>
2022-01-15 20:27:33 +0100

    Fix that Event::Handled could be set back to false

--- OloEngine/src/OloEngine/Events/Event.h
+++ OloEngine/src/OloEngine/Events/Event.h
@@ -70,7 +70,7 @@ namespace OloEngine {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(static_cast<T&>(m_Event));
+				m_Event.Handled |= func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
