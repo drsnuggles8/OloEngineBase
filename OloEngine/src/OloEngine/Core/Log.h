ab83f6f3571fee864399cdd07c5054922b1ffa47
personal <ole.bueker@gmail.com>
2022-01-15 19:13:14 +0100

    Hide compiler warnings from spdlog

--- OloEngine/src/OloEngine/Core/Log.h
+++ OloEngine/src/OloEngine/Core/Log.h
@@ -2,9 +2,12 @@
 
 #include "OloEngine/Core/Base.h"
 
+// This ignores all warnings raised inside External headers
+#pragma warning(push, 0)
 #include <spdlog/spdlog.h>
 #include <spdlog/fmt/ostr.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#pragma warning(pop)
 
 namespace OloEngine {
 	class Log
@@ -26,11 +29,11 @@ namespace OloEngine {
 #define OLO_CORE_INFO(...)    ::OloEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define OLO_CORE_WARN(...)    ::OloEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define OLO_CORE_ERROR(...)   ::OloEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define OLO_CORE_FATAL(...)   ::OloEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#define OLO_CORE_CRITICAL(...)::OloEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 // Client log macros
 #define OLO_TRACE(...)        ::OloEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define OLO_INFO(...)		  ::OloEngine::Log::GetClientLogger()->info(__VA_ARGS__)
 #define OLO_WARN(...)		  ::OloEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define OLO_ERROR(...)		  ::OloEngine::Log::GetClientLogger()->error(__VA_ARGS__)
-#define OLO_FATAL(...)		  ::OloEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
+#define OLO_CRITICAL(...)	  ::OloEngine::Log::GetClientLogger()->critical(__VA_ARGS__)
