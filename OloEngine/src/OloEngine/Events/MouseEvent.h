2ded7f5041dd5d21d38e39f0193bc063a266a449
personal <ole.bueker@gmail.com>
2022-01-15 19:11:48 +0100

    Add EventCategoryMouseButton to MouseButtonEvent

--- OloEngine/src/OloEngine/Events/MouseEvent.h
+++ OloEngine/src/OloEngine/Events/MouseEvent.h
@@ -53,7 +53,7 @@ namespace OloEngine {
 	public:
 		MouseCode GetMouseButton() const { return m_Button; }
 
-		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput)
+		EVENT_CLASS_CATEGORY(EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton)
 	protected:
 		MouseButtonEvent(const MouseCode button)
 			: m_Button(button) {}
