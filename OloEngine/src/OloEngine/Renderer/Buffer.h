b3b52eff7e5a3045196f20f2f99e47d2b15546f1
personal <ole.bueker@gmail.com>
2022-01-15 20:29:49 +0100

    Small maintenance

--- OloEngine/src/OloEngine/Renderer/Buffer.h
+++ OloEngine/src/OloEngine/Renderer/Buffer.h
@@ -70,7 +70,7 @@ namespace OloEngine {
 	public:
 		BufferLayout() = default;
 
-		BufferLayout(const std::initializer_list<BufferElement>& elements)
+		BufferLayout(std::initializer_list<BufferElement> elements)
 			: m_Elements(elements)
 		{
 			CalculateOffsetsAndStride();
