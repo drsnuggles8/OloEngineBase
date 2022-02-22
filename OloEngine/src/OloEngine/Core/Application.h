11973f3be3347daac2ad9b59ebbd096049c67149
ole <ole.bueker@gmail.com>
2022-02-22 15:54:25 +0100

    Changed std::unique_ptr to Scope in Application.h

--- OloEngine/src/OloEngine/Core/Application.h
+++ OloEngine/src/OloEngine/Core/Application.h
@@ -37,7 +37,7 @@ namespace OloEngine {
 		bool OnWindowClose(WindowCloseEvent& e);
 		bool OnWindowResize(WindowResizeEvent& e);
 	private:
-		std::unique_ptr<Window> m_Window;
+		Scope<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		bool m_Minimized = false;
