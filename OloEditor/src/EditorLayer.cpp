3006c5ad7dc782486bf797087705ac6c15415d12
ole <ole.bueker@gmail.com>
2022-05-26 19:52:27 +0200

    First refactor of framebuffer

--- OloEditor/src/EditorLayer.cpp
+++ OloEditor/src/EditorLayer.cpp
@@ -29,6 +29,7 @@ namespace OloEngine {
 		m_CheckerboardTexture = Texture2D::Create("assets/textures/Checkerboard.png");
 
 		FramebufferSpecification fbSpec;
+		fbSpec.Attachments = { FramebufferTextureFormat::RGBA8, FramebufferTextureFormat::Depth };
 		fbSpec.Width = 1280;
 		fbSpec.Height = 720;
 		m_Framebuffer = Framebuffer::Create(fbSpec);
