99f44a63e327618d393f0313fc3fe1a854a17323
ole <ole.bueker@gmail.com>
2022-06-02 10:46:29 +0200

    Small cmake fixes, improved generation batch script

--- scripts/Win-GenerateProject.bat
+++ scripts/Win-GenerateProject.bat
@@ -1,5 +1,5 @@
 @echo off
 pushd %~dp0\..\
-cmake -Hc:/Users/ole/source/repos/OloEngineBase -Bc:/Users/ole/source/repos/OloEngineBase/build -G "Visual Studio 17 2022" -DCMAKE_GENERATOR_PLATFORM=x64
+cmake -Bbuild -G "Visual Studio 17 2022" -DCMAKE_GENERATOR_PLATFORM=x64
 popd
 PAUSE
