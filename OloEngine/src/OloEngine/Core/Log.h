00e4a60586b378fe3eed450d584681751b20c1e4
ole <ole.bueker@gmail.com>
2022-06-08 19:03:24 +0200

    Added logging glm vectors, matrix and quaternions

--- OloEngine/src/OloEngine/Core/Log.h
+++ OloEngine/src/OloEngine/Core/Log.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/gtx/string_cast.hpp>
+
 #include "OloEngine/Core/Base.h"
 
 // This ignores all warnings raised inside External headers
@@ -22,6 +24,24 @@ namespace OloEngine {
 		static Ref<spdlog::logger> s_ClientLogger;
 	};
 
+}
+
+template<typename OStream, glm::length_t L, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::vec<L, T, Q>& vector)
+{
+	return os << glm::to_string(vector);
+}
+
+template<typename OStream, glm::length_t C, glm::length_t R, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, const glm::mat<C, R, T, Q>& matrix)
+{
+	return os << glm::to_string(matrix);
+}
+
+template<typename OStream, typename T, glm::qualifier Q>
+inline OStream& operator<<(OStream& os, glm::qua<T, Q> quaternion)
+{
+	return os << glm::to_string(quaternion);
 }
 
 // Core log macros
