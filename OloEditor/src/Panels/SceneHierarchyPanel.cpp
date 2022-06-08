69bc9f831a9a2a2d9f2030ce4b29ef49af58355c
ole <ole.bueker@gmail.com>
2022-06-08 19:26:21 +0200

    Stability + bug fixes

--- OloEditor/src/Panels/SceneHierarchyPanel.cpp
+++ OloEditor/src/Panels/SceneHierarchyPanel.cpp
@@ -340,7 +340,11 @@ namespace OloEngine {
 				{
 					const wchar_t* path = (const wchar_t*)payload->Data;
 					std::filesystem::path texturePath = std::filesystem::path(g_AssetPath) / path;
-					component.Texture = Texture2D::Create(texturePath.string());
+					Ref<Texture2D> texture = Texture2D::Create(texturePath.string());
+					if (texture->IsLoaded())
+						component.Texture = texture;
+					else
+						OLO_WARN("Could not load texture {0}", texturePath.filename().string());
 				}
 				ImGui::EndDragDropTarget();
 			}
--- OloEngine/src/OloEngine/Scene/SceneSerializer.cpp
+++ OloEngine/src/OloEngine/Scene/SceneSerializer.cpp
@@ -185,7 +185,16 @@ namespace OloEngine {
 
 	bool SceneSerializer::Deserialize(const std::string& filepath)
 	{
-		YAML::Node data = YAML::LoadFile(filepath);
+		YAML::Node data;
+		try
+		{
+			data = YAML::LoadFile(filepath);
+		}
+		catch (YAML::ParserException e)
+		{
+			return false;
+		}
+
 		if (!data["Scene"])
 			return false;
 
