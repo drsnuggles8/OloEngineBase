a03485d6f7d24607d1811d77c95275665fcfc8fe
ole <ole.bueker@gmail.com>
2022-06-08 21:37:53 +0200

    Drag a texture image directly into the viewport

--- OloEditor/src/EditorLayer.cpp
+++ OloEditor/src/EditorLayer.cpp
@@ -287,7 +287,26 @@ namespace OloEngine {
 			if (const ImGuiPayload* payload = ImGui::AcceptDragDropPayload("CONTENT_BROWSER_ITEM"))
 			{
 				const wchar_t* path = (const wchar_t*)payload->Data;
-				OpenScene(std::filesystem::path(g_AssetPath) / path);
+
+				std::filesystem::path filePath = std::filesystem::path(path);
+				if (filePath.extension().string() == ".olo")
+				{
+					OpenScene(std::filesystem::path(g_AssetPath) / path);
+				}
+				else if (filePath.extension().string() == ".png")
+				{
+					std::filesystem::path texturePath = std::filesystem::path(g_AssetPath) / path;
+					Ref<Texture2D> texture = Texture2D::Create(texturePath.string());
+					if (texture->IsLoaded())
+					{
+						if (m_HoveredEntity && m_HoveredEntity.HasComponent<SpriteRendererComponent>())
+							m_HoveredEntity.GetComponent<SpriteRendererComponent>().Texture = texture;
+					}
+					else
+					{
+						OLO_WARN("Could not load texture {0}", texturePath.filename().string());
+					}
+				}
 			}
 			ImGui::EndDragDropTarget();
 		}
