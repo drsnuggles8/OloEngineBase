a8c36f80e9136a1eac302833542ba22bfa06450f
ole <ole.bueker@gmail.com>
2022-06-08 18:43:41 +0200

    Added play/stop button

--- OloEditor/src/EditorLayer.h
+++ OloEditor/src/EditorLayer.h
@@ -30,6 +30,12 @@ namespace OloEngine {
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
 
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
+
 	private:
 		OloEngine::OrthographicCameraController m_CameraController;
 
@@ -60,9 +66,18 @@ namespace OloEngine {
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0, Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 
 }
