683fa97c481fff7f0becdce3e5f6eff20a137aa7
ole <ole.bueker@gmail.com>
2022-06-10 19:18:56 +0200

    Added TODO, finish the branch for now

--- OloEngine/test/OloEngineTest.cpp
+++ OloEngine/test/OloEngineTest.cpp
@@ -2,6 +2,7 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "OloEnginePCH.h"
 #include <gtest/gtest.h>
+//TODO: Implement more tests, look at https://github.com/turanszkij/WickedEngine/blob/master/Tests/Tests.cpp for some ideas
 
 int main(int argc, char** argv)
 {
