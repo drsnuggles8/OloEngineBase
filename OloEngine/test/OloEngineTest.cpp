3357412fb7a610c8dd20b3b75f3fe77509e9262b
ole <ole.bueker@gmail.com>
2022-06-10 19:46:29 +0200

    Code smell removal

--- OloEngine/test/OloEngineTest.cpp
+++ OloEngine/test/OloEngineTest.cpp
@@ -2,10 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "OloEnginePCH.h"
 #include <gtest/gtest.h>
-//TODO: Implement more tests, look at https://github.com/turanszkij/WickedEngine/blob/master/Tests/Tests.cpp for some ideas
+//TODO(olbu): Implement more tests, look at https://github.com/turanszkij/WickedEngine/blob/master/Tests/Tests.cpp for some ideas
 
 int main(int argc, char** argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	return ::RUN_ALL_TESTS();
 }
