6916a0ec89fcc9d03bd8623f46269893ae0c3171
ole <ole.bueker@gmail.com>
2022-06-15 11:35:39 +0200

    Cleaning the SceneHierarchyPanel

--- OloEditor/src/Panels/SceneHierarchyPanel.cpp
+++ OloEditor/src/Panels/SceneHierarchyPanel.cpp
@@ -10,9 +10,9 @@
 #include "OloEngine/Scene/Components.h"
 
 #include <cstring>
-/* The Microsoft C++ compiler is non-compliant with the C++ standard and needs
- * the following definition to disable a security warning on std::strncpy().
- */
+// The Microsoft C++ compiler is non-compliant with the C++ standard and needs
+// the following definition to disable a security warning on std::strncpy().
+//
 #ifdef _MSVC_LANG
 	#define _CRT_SECURE_NO_WARNINGS
 #endif
@@ -43,13 +43,17 @@ namespace OloEngine {
 		});
 
 		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+		{
 			m_SelectionContext = {};
+		}
 
 		// Right-click on blank space
 		if (ImGui::BeginPopupContextWindow(0, 1, false))
 		{
 			if (ImGui::MenuItem("Create Empty Entity"))
+			{
 				m_Context->CreateEntity("Empty Entity");
+			}
 
 			ImGui::EndPopup();
 		}
@@ -65,7 +69,7 @@ namespace OloEngine {
 		ImGui::End();
 	}
 
-	void SceneHierarchyPanel::SetSelectedEntity(Entity entity)
+	void SceneHierarchyPanel::SetSelectedEntity(const Entity entity)
 	{
 		m_SelectionContext = entity;
 	}
@@ -74,10 +78,10 @@ namespace OloEngine {
 	{
 		auto& tagComponent = entity.GetComponent<TagComponent>();
 		auto& tag = tagComponent.Tag;
-		
+
 		ImGuiTreeNodeFlags flags = ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow;
 		flags |= ImGuiTreeNodeFlags_SpanAvailWidth;
-		bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
+		const bool opened = ImGui::TreeNodeEx((void*)(uint64_t)(uint32_t)entity, flags, tag.c_str());
 		if (ImGui::IsItemClicked())
 		{
 			m_SelectionContext = entity;
@@ -92,7 +96,9 @@ namespace OloEngine {
 			}
 
 			if (ImGui::MenuItem("Delete Entity"))
+			{
 				entityDeleted = true;
+			}
 
 			ImGui::EndPopup();
 		}
@@ -120,10 +126,12 @@ namespace OloEngine {
 
 		if (opened)
 		{
-			ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_SpanAvailWidth;
-			bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
+			const ImGuiTreeNodeFlags flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_SpanAvailWidth;
+			const bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
 			if (opened)
+			{
 				ImGui::TreePop();
+			}
 			ImGui::TreePop();
 		}
 
@@ -131,14 +139,16 @@ namespace OloEngine {
 		{
 			m_Context->DestroyEntity(entity);
 			if (m_SelectionContext == entity)
+			{
 				m_SelectionContext = {};
+			}
 		}
 	}
 
 	static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f)
 	{
 		ImGuiIO& io = ImGui::GetIO();
-		auto boldFont = io.Fonts->Fonts[0];
+		const auto boldFont = io.Fonts->Fonts[0];
 
 		ImGui::PushID(label.c_str());
 
@@ -150,15 +160,17 @@ namespace OloEngine {
 		ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
 		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
 
-		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
-		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+		const float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		const ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
 
 		ImGui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
 		ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
 		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
 		ImGui::PushFont(boldFont);
 		if (ImGui::Button("X", buttonSize))
+		{
 			values.x = resetValue;
+		}
 		ImGui::PopFont();
 		ImGui::PopStyleColor(3);
 
@@ -172,7 +184,9 @@ namespace OloEngine {
 		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
 		ImGui::PushFont(boldFont);
 		if (ImGui::Button("Y", buttonSize))
+		{
 			values.y = resetValue;
+		}
 		ImGui::PopFont();
 		ImGui::PopStyleColor(3);
 
@@ -186,7 +200,9 @@ namespace OloEngine {
 		ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
 		ImGui::PushFont(boldFont);
 		if (ImGui::Button("Z", buttonSize))
+		{
 			values.z = resetValue;
+		}
 		ImGui::PopFont();
 		ImGui::PopStyleColor(3);
 
@@ -200,7 +216,7 @@ namespace OloEngine {
 
 		ImGui::PopID();
 	}
-	
+
 	template<typename T, typename UIFunction>
 	static void DrawComponent(const std::string& name, Entity entity, UIFunction uiFunction)
 	{
@@ -208,12 +224,12 @@ namespace OloEngine {
 		if (entity.HasComponent<T>())
 		{
 			auto& component = entity.GetComponent<T>();
-			ImVec2 contentRegionAvailable = ImGui::GetContentRegionAvail();
+			const ImVec2 contentRegionAvailable = ImGui::GetContentRegionAvail();
 
 			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2{ 4, 4 });
-			float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+			const float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
 			ImGui::Separator();
-			bool open = ImGui::TreeNodeEx((void*)typeid(T).hash_code(), treeNodeFlags, name.c_str());
+			const bool open = ImGui::TreeNodeEx((void*)typeid(T).hash_code(), treeNodeFlags, name.c_str());
 			ImGui::PopStyleVar(
 			);
 			ImGui::SameLine(contentRegionAvailable.x - lineHeight * 0.5f);
@@ -226,7 +242,9 @@ namespace OloEngine {
 			if (ImGui::BeginPopup("ComponentSettings"))
 			{
 				if (ImGui::MenuItem("Remove component"))
+				{
 					removeComponent = true;
+				}
 
 				ImGui::EndPopup();
 			}
@@ -238,7 +256,9 @@ namespace OloEngine {
 			}
 
 			if (removeComponent)
+			{
 				entity.RemoveComponent<T>();
+			}
 		}
 	}
 
@@ -261,7 +281,9 @@ namespace OloEngine {
 		ImGui::PushItemWidth(-1);
 
 		if (ImGui::Button("Add Component"))
+		{
 			ImGui::OpenPopup("AddComponent");
+		}
 
 		if (ImGui::BeginPopup("AddComponent"))
 		{
