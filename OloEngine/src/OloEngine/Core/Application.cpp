dc57fbebb93ba6de215f179005e0cf1e9fe4d122
ole <ole.bueker@gmail.com>
2022-07-24 14:55:58 +0200

    Code smells better

--- OloEngine/src/OloEngine/Core/Application.cpp
+++ OloEngine/src/OloEngine/Core/Application.cpp
@@ -22,7 +22,9 @@ namespace OloEngine {
 		s_Instance = this;
 		// Set working directory here
 		if (!m_Specification.WorkingDirectory.empty())
+		{
 			std::filesystem::current_path(m_Specification.WorkingDirectory);
+		}
 
 		m_Window = Window::Create(WindowProps(m_Specification.Name));
 		m_Window->SetEventCallback(OLO_BIND_EVENT_FN(Application::OnEvent));
