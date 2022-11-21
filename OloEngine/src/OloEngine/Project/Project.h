68026adef43ef80930c34620c50097d951fb9408
ole <ole.bueker@gmail.com>
2022-11-21 23:30:16 +0100

    Added basic files for project (not integrated)

---
+++ OloEngine/src/OloEngine/Project/Project.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "OloEngine/Core/Base.h"
+
+#include <string>
+#include <filesystem>
+
+namespace OloEngine
+{
+
+	struct ProjectConfig
+	{
+		std::string Name = "Untitled";
+
+		std::filesystem::path StartScene;
+
+		std::filesystem::path AssetDirectory;
+		std::filesystem::path ScriptModulePath;
+	};
+
+	class Project
+	{
+	public:
+		static const std::filesystem::path& GetProjectDirectory()
+		{
+			OLO_CORE_ASSERT(s_ActiveProject);
+			return s_ActiveProject->m_ProjectDirectory;
+		}
+
+		static std::filesystem::path GetAssetDirectory()
+		{
+			OLO_CORE_ASSERT(s_ActiveProject);
+			return GetProjectDirectory() / s_ActiveProject->m_Config.AssetDirectory;
+		}
+
+		// TODO(olbu): move to asset manager when we have one
+		static std::filesystem::path GetAssetFileSystemPath(const std::filesystem::path& path)
+		{
+			OLO_CORE_ASSERT(s_ActiveProject);
+			return GetAssetDirectory() / path;
+		}
+
+		ProjectConfig& GetConfig() { return m_Config; }
+
+		static Ref<Project> GetActive() { return s_ActiveProject; }
+
+		static Ref<Project> New();
+		static Ref<Project> Load(const std::filesystem::path& path);
+		static bool SaveActive(const std::filesystem::path& path);
+	private:
+		ProjectConfig m_Config;
+		std::filesystem::path m_ProjectDirectory;
+
+		inline static Ref<Project> s_ActiveProject;
+	};
+
+}
---
+++ OloEngine/src/OloEngine/Project/ProjectSerializer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "OloEngine/Project/Project.h"
+
+namespace OloEngine
+{
+
+	class ProjectSerializer
+	{
+	public:
+		ProjectSerializer(Ref<Project> project);
+
+		bool Serialize(const std::filesystem::path& filepath);
+		bool Deserialize(const std::filesystem::path& filepath);
+	private:
+		Ref<Project> m_Project;
+	};
+
+}
