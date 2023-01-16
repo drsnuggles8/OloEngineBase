9547289a7d9978a4720cfb588f6353efa7b23d6d
ole <ole.bueker@gmail.com>
2023-01-16 21:58:58 +0100

    Revert "Added texture2array, reworked framebuffer to work with it (currently not working)"
    This reverts commit 009f7cfff30d270add53a8ba37e95242f8276c7b.

--- OloEngine/src/Platform/OpenGL/OpenGLTexture.h
+++ OloEngine/src/Platform/OpenGL/OpenGLTexture.h
@@ -38,38 +38,4 @@ namespace OloEngine
 		GLenum m_DataFormat;
 	};
 
-	class OpenGLTexture2DArray : public Texture2DArray
-	{
-	public:
-		OpenGLTexture2DArray(std::vector<uint32_t> widths, std::vector<uint32_t> heights);
-		explicit OpenGLTexture2DArray(const std::vector<std::string>& paths);
-		~OpenGLTexture2DArray() override;
-
-		[[nodiscard("Store this!")]] std::vector<uint32_t> GetWidths() const override { return m_Widths; }
-		[[nodiscard("Store this!")]] std::vector<uint32_t> GetHeights() const override { return m_Heights; }
-		[[nodiscard("Store this!")]] uint32_t GetLayers() const override { return m_Layers; }
-		[[nodiscard("Store this!")]] uint32_t GetRendererID() const override { return m_RendererID; }
-		[[nodiscard("Store this!")]] const std::vector<std::string>& GetPaths() const override { return m_Paths; }
-
-		void SetLayerData(void* data, uint32_t size, uint32_t layer) override;
-
-		void Bind(const uint32_t slot) const override;
-		void BindLayer(const uint32_t slot, const uint32_t layer) const override;
-
-		[[nodiscard("Store this!")]] bool IsLoaded() const override { return m_IsLoaded; }
-
-		bool operator==(const Texture& other) const override
-		{
-			return m_RendererID == other.GetRendererID();
-		}
-	private:
-		std::vector<std::string> m_Paths;
-		bool m_IsLoaded = false;
-		std::vector<uint32_t> m_Widths;
-		std::vector<uint32_t> m_Heights;
-		uint32_t m_Layers;
-		uint32_t m_RendererID{};
-		GLenum m_InternalFormat;
-		GLenum m_DataFormat;
-	};
 }
