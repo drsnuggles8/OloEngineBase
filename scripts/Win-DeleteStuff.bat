7f7eb7e0eec33f19dc3c6f3af60d65e6fe48c86b
ole <ole.bueker@gmail.com>
2023-06-23 13:13:53 +0200

    Improved delete script

--- scripts/Win-DeleteStuff.bat
+++ scripts/Win-DeleteStuff.bat
@@ -1,6 +1,17 @@
 @echo off
 pushd %~dp0\..\
-RMDIR "bin" /S /Q
-RMDIR "build" /S /Q
+if exist "bin" (
+    RMDIR "bin" /S /Q
+)
+if exist "build" (
+    RMDIR "build" /S /Q
+)
+if exist "OloEngine\vendor" (
+    for /d %%i in ("OloEngine\vendor\*") do (
+        if /i not "%%i"=="OloEngine\vendor\CMakeLists.txt" (
+            RMDIR "%%i" /S /Q
+        )
+    )
+)
 popd
 PAUSE
