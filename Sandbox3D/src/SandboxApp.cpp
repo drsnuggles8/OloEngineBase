354b0369a5806efabe003824ccba84bc42d061a8
Ole Bueker <ole.bueker@gmail.com>
2025-02-25 19:35:34 +0100

    3d project created

---
+++ Sandbox3D/src/SandboxApp.cpp
@@ -0,0 +1,26 @@
+#include <OloEngine.h>
+#include <OloEngine/Core/EntryPoint.h>
+
+#include "Sandbox3D.h"
+
+class Sandbox : public OloEngine::Application
+{
+public:
+	Sandbox(const OloEngine::ApplicationSpecification& specification)
+		: OloEngine::Application(specification)
+	{
+		PushLayer(new Sandbox3D());
+	}
+
+	~Sandbox() final = default;
+};
+
+OloEngine::Application* OloEngine::CreateApplication(const OloEngine::ApplicationCommandLineArgs args)
+{
+	ApplicationSpecification spec;
+	spec.Name = "Sandbox";
+	spec.WorkingDirectory = "../OloEditor";
+	spec.CommandLineArgs = args;
+
+	return new Sandbox(spec);
+}
