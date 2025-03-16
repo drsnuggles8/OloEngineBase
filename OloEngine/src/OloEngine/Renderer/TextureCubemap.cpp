2f11dffc33ab52f867bae0ab400ba9e38af654bb
Ole Bueker <ole.bueker@gmail.com>
2025-03-16 21:49:22 +0100

    Add TextureCubemap class and OpenGL implementation for cubemap textures

---
+++ OloEngine/src/OloEngine/Renderer/TextureCubemap.cpp
@@ -0,0 +1,48 @@
+#include "OloEnginePCH.h"
+#include "OloEngine/Renderer/TextureCubemap.h"
+
+#include "OloEngine/Renderer/Renderer.h"
+#include "Platform/OpenGL/OpenGLTextureCubemap.h"
+
+namespace OloEngine
+{
+    Ref<TextureCubemap> TextureCubemap::Create(const std::vector<std::string>& facePaths)
+    {
+        OLO_CORE_ASSERT(facePaths.size() == 6, "Cubemap requires exactly 6 face textures!");
+        
+        switch (Renderer::GetAPI())
+        {
+            case RendererAPI::API::None:
+            {
+                OLO_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+                return nullptr;
+            }
+            case RendererAPI::API::OpenGL:
+            {
+                return CreateRef<OpenGLTextureCubemap>(facePaths);
+            }
+        }
+
+        OLO_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+
+    Ref<TextureCubemap> TextureCubemap::Create(const CubemapSpecification& specification)
+    {
+        switch (Renderer::GetAPI())
+        {
+            case RendererAPI::API::None:
+            {
+                OLO_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+                return nullptr;
+            }
+            case RendererAPI::API::OpenGL:
+            {
+                return CreateRef<OpenGLTextureCubemap>(specification);
+            }
+        }
+
+        OLO_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+}
